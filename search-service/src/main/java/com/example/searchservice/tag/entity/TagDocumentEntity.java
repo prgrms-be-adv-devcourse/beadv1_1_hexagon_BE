69dfd7e09a93e7fdf9265b66cfdf29e1f5c76e0f
chahuigwang <chahuigwang@gmail.com>
2025-11-12 15:08:59 +0900

    feat(search): Tag 자동 완성을 위한 completion 필드추가 + tag 별칭 사전(json)

--- search-service/src/main/java/com/example/searchservice/tag/entity/TagDocumentEntity.java
+++ search-service/src/main/java/com/example/searchservice/tag/entity/TagDocumentEntity.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.CompletionField;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.core.suggest.Completion;
 
 @Getter
 @Setter
@@ -23,4 +25,8 @@ public class TagDocumentEntity {
 
     @Field(type = FieldType.Keyword)
     private String skill;
+
+    @Field(name = "skill_suggest")
+    @CompletionField
+    private Completion skillSuggest;
 }
