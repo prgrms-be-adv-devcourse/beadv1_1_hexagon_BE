91c8bbb2d4fcd2a6b94079a3387952187e2c4c98
chahuigwang <chahuigwang@gmail.com>
2025-11-13 20:34:42 +0900

    chore(search): TagIndexInitializer 테스트용 메소드로 교체(해당 이슈와 관련 없음)

--- search-service/src/main/java/com/example/searchservice/tag/service/TagIndexInitializer.java
+++ search-service/src/main/java/com/example/searchservice/tag/service/TagIndexInitializer.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.elasticsearch.core.suggest.Completion;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -28,28 +29,56 @@ public class TagIndexInitializer {
     @Value("${external.profile-service.url}")
     private String profileServiceUrl;
 
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void initIndex() {
+//        String getAllTagsUrl = profileServiceUrl + "/api/tags";
+//        ResponseEntity<List<ProfileTagDto>> response = restTemplate.exchange(
+//                getAllTagsUrl,
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<>() {
+//                }
+//        );
+//
+//        List<ProfileTagDto> tags = response.getBody();
+//
+//        if (tags == null || tags.isEmpty()) {
+//            throw new TagException(TagErrorCode.TAG_FETCH_FAILED);
+//        }
+//
+//        for (ProfileTagDto tagDto : tags) {
+//            List<String> aliases = tagAliasLoadService.getTagAlias(tagDto.skill());
+//
+//            TagDocumentEntity document = TagMapper.toDocument(tagDto, aliases);
+//            tagRepository.save(document);
+//        }
+//    }
+
+    // 테스트용 메소드
     @EventListener(ApplicationReadyEvent.class)
     public void initIndex() {
-        String getAllTagsUrl = profileServiceUrl + "/api/tags";
-        ResponseEntity<List<ProfileTagDto>> response = restTemplate.exchange(
-                getAllTagsUrl,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<>() {
-                }
-        );
+        tagRepository.save(TagDocumentEntity.builder()
+                .code("tag-001")
+                .skill("Spring")
+                .skillSuggest(new Completion(new String[]{"Spring", "spring", "스프링"}))
+                .build());
 
-        List<ProfileTagDto> tags = response.getBody();
+        tagRepository.save(TagDocumentEntity.builder()
+                .code("tag-002")
+                .skill("Spring Boot")
+                .skillSuggest(new Completion(new String[]{"Spring Boot", "spring boot", "스프링 부트", "스프링부트"}))
+                .build());
 
-        if (tags == null || tags.isEmpty()) {
-            throw new TagException(TagErrorCode.TAG_FETCH_FAILED);
-        }
+        tagRepository.save(TagDocumentEntity.builder()
+                .code("tag-003")
+                .skill("Java")
+                .skillSuggest(new Completion(new String[]{"Java", "java", "자바"}))
+                .build());
 
-        for (ProfileTagDto tagDto : tags) {
-            List<String> aliases = tagAliasLoadService.getTagAlias(tagDto.skill());
-
-            TagDocumentEntity document = TagMapper.toDocument(tagDto, aliases);
-            tagRepository.save(document);
-        }
+        tagRepository.save(TagDocumentEntity.builder()
+                .code("tag-004")
+                .skill("JavaScript")
+                .skillSuggest(new Completion(new String[]{"JavaScript", "javascript", "js", "자바스크립트"}))
+                .build());
     }
 }
