b65f87cd57d78334e75d17f74b9433dbe40599ea
chahuigwang <chahuigwang@gmail.com>
2025-11-15 18:37:29 +0900

    chore(search): Swagger 설정 추가

--- search-service/src/main/java/com/example/searchservice/selfpromotion/controller/swagger/SelfPromotionControllerSwagger.java
+++ search-service/src/main/java/com/example/searchservice/selfpromotion/controller/swagger/SelfPromotionControllerSwagger.java
@@ -3,14 +3,21 @@ package com.example.searchservice.selfpromotion.controller.swagger;
 import com.example.searchservice.common.response.BaseResponse;
 import com.example.searchservice.common.vo.SearchScope;
 import com.example.searchservice.selfpromotion.dto.SelfPromotionResponseDto;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "Self Promotion Search API", description = "Self Promotion 검색 / 검색 키워드 추천 API")
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "http://localhost:8000", description = "Gateway URL")
+        }
+)
 public interface SelfPromotionControllerSwagger {
 
     @Operation(summary = "Self Promotion 검색", description = "검색어, 검색 범위를 기반으로 Self Promotion을 검색합니다.")
