eb2bdeda00a1a8f6acf33b89665765ba388387b6
chahuigwang <chahuigwang@gmail.com>
2025-11-16 21:46:20 +0900

    feat(search): cartpost-service에서 넘어올 데이터를 담기 위한 DTO 생성

---
+++ search-service/src/main/java/com/example/searchservice/commission/service/dto/CartPostCommissionDto.java
@@ -0,0 +1,23 @@
+package com.example.searchservice.commission.service.dto;
+
+import com.example.searchservice.commission.common.PaymentType;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
+
+public record CartPostCommissionDto(
+        String code,
+        String title,
+        String content,
+        String memberCode,
+        String memberNickname,
+        List<String> tags,
+        LocalDate startedAt,
+        LocalDate endedAt,
+        PaymentType paymentType,
+        Long payAmount,
+        Boolean isClosed,
+        Instant updatedAt
+) {
+
+}
