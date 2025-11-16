dfc5f4d8f0eaa17a00f50b36544ab7ad338f8118
chahuigwang <chahuigwang@gmail.com>
2025-11-16 19:15:16 +0900

    feat(search): Commission Repository 클래스 구현

--- search-service/src/main/java/com/example/searchservice/commission/repository/CommissionRepository.java
+++ search-service/src/main/java/com/example/searchservice/commission/repository/CommissionRepository.java
@@ -1,8 +1,48 @@
 package com.example.searchservice.commission.repository;
 
+import com.example.searchservice.commission.dto.CommissionResponseDto;
 import com.example.searchservice.commission.entity.CommissionDocumentEntity;
+import com.example.searchservice.selfpromotion.entity.SelfPromotionDocumentEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface CommissionRepository extends ElasticsearchRepository<CommissionDocumentEntity, String> {
 
+    @Query("""
+            {
+              "multi_match": {
+                "query": "#{#q}",
+                "fields": ["title", "content"],
+                "operator": "or",
+                "fuzziness": "1"
+              }
+            }
+            """)
+    Page<CommissionDocumentEntity> searchAll(String query, Pageable pageable);
+
+    @Query("""
+            {
+              "match": {
+                "title": {
+                  "query": "#{#q}",
+                  "fuzziness": "1"
+                }
+              }
+            }
+            """)
+    Page<CommissionDocumentEntity> searchTitle(String query, Pageable pageable);
+
+    @Query("""
+            {
+              "match": {
+                "content": {
+                  "query": "#{#q}",
+                  "fuzziness": "1"
+                }
+              }
+            }
+            """)
+    Page<CommissionDocumentEntity> searchContent(String query, Pageable pageable);
 }
