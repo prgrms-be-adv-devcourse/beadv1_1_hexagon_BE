354998a77ac351731a44ca8cbd3395c2194c40ae
chahuigwang <chahuigwang@gmail.com>
2025-11-18 16:22:46 +0900

    feat(search): 검색어 추천을 위한 search_as_you_type 서브 필드 추가

--- search-service/src/main/java/com/example/searchservice/commission/entity/CommissionDocumentEntity.java
+++ search-service/src/main/java/com/example/searchservice/commission/entity/CommissionDocumentEntity.java
@@ -15,6 +15,8 @@ import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.InnerField;
+import org.springframework.data.elasticsearch.annotations.MultiField;
 import org.springframework.data.elasticsearch.annotations.Setting;
 
 @Getter
@@ -29,10 +31,15 @@ public class CommissionDocumentEntity {
     @Id
     private String code;
 
-    @Field(type = FieldType.Text)
+    @MultiField(
+            mainField = @Field(type = FieldType.Text, analyzer = "commission_index_analyzer", searchAnalyzer = "commission_search_analyzer"),
+            otherFields = {
+                    @InnerField(suffix = "completion", type = FieldType.Search_As_You_Type)
+            }
+    )
     private String title;
 
-    @Field(type = FieldType.Text)
+    @Field(type = FieldType.Text, analyzer = "commission_index_analyzer", searchAnalyzer = "commission_search_analyzer")
     private String content;
 
     @Field(type = FieldType.Keyword)
