438c13eee00e2644754ccaed755504c901c7eabb
chahuigwang <chahuigwang@gmail.com>
2025-11-18 16:43:45 +0900

    feat(search): 의뢰글 검색어 추천 메소드에서 전체 제목 -> 키워드(명사만) 변환 로직 추가

--- search-service/src/main/java/com/example/searchservice/commission/service/CommissionServiceImpl.java
+++ search-service/src/main/java/com/example/searchservice/commission/service/CommissionServiceImpl.java
@@ -1,5 +1,9 @@
 package com.example.searchservice.commission.service;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.indices.AnalyzeRequest;
+import co.elastic.clients.elasticsearch.indices.AnalyzeResponse;
+import co.elastic.clients.elasticsearch.indices.analyze.AnalyzeToken;
 import com.example.searchservice.commission.dto.CommissionResponseDto;
 import com.example.searchservice.commission.entity.CommissionDocumentEntity;
 import com.example.searchservice.commission.repository.CommissionRepository;
@@ -18,6 +22,7 @@ import org.springframework.stereotype.Service;
 public class CommissionServiceImpl implements CommissionService {
 
     private final CommissionRepository commissionRepository;
+    private final ElasticsearchClient esClient;
 
     @Override
     public Page<CommissionResponseDto> search(String query, SearchScope scope, int page, int size) {
@@ -49,6 +54,7 @@ public class CommissionServiceImpl implements CommissionService {
         return searchHits.stream()
                 .map(SearchHit::getContent)
                 .map(CommissionDocumentEntity::getTitle)
+                .map(this::extractNounsWithEs)
                 .distinct()
                 .limit(size)
                 .toList();
@@ -90,5 +96,35 @@ public class CommissionServiceImpl implements CommissionService {
     @Override
     public void delete(String code) {
         commissionRepository.deleteById(code);
+    }
+
+    // ES에 _analyze 요청 보내 "korean_noun_analyzer" 통해 명사만 남기는 로직
+    private String extractNounsWithEs(String text) {
+        if (text == null || text.isBlank()) {
+            return "";
+        }
+
+        try {
+            AnalyzeRequest req = AnalyzeRequest.of(a -> a
+                    .index("commissions")            // 인덱스 이름
+                    .analyzer("korean_noun_analyzer") // 네가 정의한 analyzer
+                    .text(text)
+            );
+
+            AnalyzeResponse response = esClient
+                    .indices()
+                    .analyze(req);
+
+            List<String> tokens = response.tokens()
+                    .stream()
+                    .map(AnalyzeToken::token)
+                    .toList();
+
+            return String.join(" ", tokens);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
