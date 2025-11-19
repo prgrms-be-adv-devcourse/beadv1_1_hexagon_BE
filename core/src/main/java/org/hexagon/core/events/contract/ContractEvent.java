cc571dfb2b99b13bbf7eefeca9344991b3455b0a
chahuigwang <chahuigwang@gmail.com>
2025-11-19 19:03:41 +0900

    chore(core): ContractEvent에 memberCode 필드 추가

--- core/src/main/java/org/hexagon/core/events/contract/ContractEvent.java
+++ core/src/main/java/org/hexagon/core/events/contract/ContractEvent.java
@@ -3,6 +3,7 @@ package org.hexagon.core.events.contract;
 import java.time.Instant;
 
 public record ContractEvent(
+        String memberCode,
         String contractCode,
         Instant createdAt,
         String status
