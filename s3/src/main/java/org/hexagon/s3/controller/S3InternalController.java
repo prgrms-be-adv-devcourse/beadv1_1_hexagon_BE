9dc001b43e4f1e5977987101dca38ed1493c1a1f
chahuigwang <chahuigwang@gmail.com>
2025-12-03 12:02:05 +0900

    chore(s3): Controller -> RestController로 수정

--- s3/src/main/java/org/hexagon/s3/controller/S3InternalController.java
+++ s3/src/main/java/org/hexagon/s3/controller/S3InternalController.java
@@ -7,12 +7,12 @@ import org.hexagon.s3.dto.PresignedUploadRequest;
 import org.hexagon.s3.dto.PresignedUploadResponse;
 import org.hexagon.s3.dto.ServiceName;
 import org.hexagon.s3.service.S3Service;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/internal/s3")
 public class S3InternalController {
