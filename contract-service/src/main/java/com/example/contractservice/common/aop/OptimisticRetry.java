119a2e2b0c6b1780297ee8f160900eb577eba29f
ZZAMBAs <rksidksrksi@naver.com>
2025-12-04 10:32:18 +0900

    feat(contract): 계속된 충돌 방지를 위해 지터를 추가

--- contract-service/src/main/java/com/example/contractservice/common/aop/OptimisticRetry.java
+++ contract-service/src/main/java/com/example/contractservice/common/aop/OptimisticRetry.java
@@ -6,6 +6,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.core.annotation.AliasFor;
 
+/**
+ * 낙관적 락에 의한 재시도를 위해 만들어진 어노테이션. <br />
+ * 각각은 다음 의미를 지닙니다. <br />
+ * - maxAttempts: 최대 재시도 횟수 <br />
+ * - delay: 재시도 간 최소 간격 <br />
+ * - jitter: delay에 추가되는 최대 지연 시간 <br />
+ * <br />
+ * 만약에 delay가 200이고 jitter가 100이면 (200 + @)ms 의 시간동안 지연된 뒤, 재시도합니다. 이때 @는 jitter 값을 넘지 않는 음이 아닌 랜덤 정수입니다.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OptimisticRetry {
@@ -15,6 +24,8 @@ public @interface OptimisticRetry {
     @AliasFor("value")
     int maxAttempts() default 3;
 
-    long delay() default 200; // millisecond
+    int delay() default 300; // millisecond
+
+    int jitter() default 150; // millisecond
 
 }
--- contract-service/src/main/java/com/example/contractservice/common/aop/OptimisticRetryAspect.java
+++ contract-service/src/main/java/com/example/contractservice/common/aop/OptimisticRetryAspect.java
@@ -1,6 +1,7 @@
 package com.example.contractservice.common.aop;
 
 import jakarta.persistence.OptimisticLockException;
+import java.security.SecureRandom;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -15,25 +16,32 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 @Order(10) // 따로 지정하지 않으면 최하위 순서가 되며 이는 @Transactional과 같다. 허나 같으면 @Transactional이 먼저 적용된다. 왜일까
-public class OptimisticRetryAspect {
+public class OptimisticRetryAspect { // TODO: ThreadLocal을 이용해 상위 어노테이션과 통합
+    private static final SecureRandom RANDOM = new SecureRandom();
 
     @Around("@annotation(retryOptions)")
     public Object retry(ProceedingJoinPoint joinPoint, OptimisticRetry retryOptions) throws Throwable {
         log.info("시도 시작!");
         int retryCount = 0;
+        int jitter = retryOptions.jitter();
         int maxAttempts = retryOptions.maxAttempts();
-        long delayMillis = retryOptions.delay();
+        int delayMillis = retryOptions.delay();
+        Throwable throwable = null;
 
         while (retryCount < maxAttempts) {
             try {
                 return joinPoint.proceed();
-            } catch (ObjectOptimisticLockingFailureException | OptimisticLockException | StaleObjectStateException e) { // 낙관적 락 예외만 재시도한다.
+            } catch (ObjectOptimisticLockingFailureException | OptimisticLockException | StaleObjectStateException e) {
                 retryCount++;
+                Thread.sleep(delayMillis + RANDOM.nextInt(jitter));
                 log.warn("낙관적 락 예외로 인해 재시도 중입니다... 현재 재시도 횟수: {}, 전체 재시도 횟수: {}", retryCount, maxAttempts);
-            } // 그 외 예외는 재시도 없이 바로 던져진다.
 
-            Thread.sleep(delayMillis);
+                if (retryCount == maxAttempts) {
+                    throwable = e;
+                }
+
+            } // 그 외 예외는 재시도 없이 바로 던져진다.
         }
-        throw new OptimisticLockingFailureException("낙관적 락 예외, 최대 재시도 횟수 초과");
+        throw new OptimisticLockingFailureException("낙관적 락 예외, 최대 재시도 횟수 초과", throwable);
     }
 }
