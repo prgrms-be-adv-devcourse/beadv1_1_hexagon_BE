be22160d3641389a443668ef502736d9ef94c147
chahuigwang <chahuigwang@gmail.com>
2025-12-05 15:26:12 +0900

    chore(s3): 사용하지 않는 API 주석 삭제

--- s3-service/src/main/java/org/hexagon/s3service/controller/S3Controller.java
+++ s3-service/src/main/java/org/hexagon/s3service/controller/S3Controller.java
@@ -36,19 +36,6 @@ public class S3Controller {
         return ResponseDto.success(response);
     }
 
-    // TODO: 모든 API 구현 완료 후 주석 삭제
-//    @PostMapping("/download-url")
-//    public ResponseDto<PresignedDownloadResponse> getDownloadUrl(@RequestBody PresignedDownloadRequest request) {
-//        PresignedDownloadResponse downloadUrl = s3Service.createDownloadUrl(request.key());
-//        return ResponseDto.success(downloadUrl);
-//    }
-
-//    @PostMapping("/download-urls")
-//    public ResponseDto<PresignedDownloadListResponse> getDownloadUrls(@RequestBody PresignedDownloadRequestByKey request) {
-//        PresignedDownloadListResponse downloadUrls = s3Service.createDownloadUrls(request.keys());
-//        return ResponseDto.success(downloadUrls);
-//    }
-
     @DeleteMapping
     public ResponseDto<Empty> deleteObject(@RequestParam String key) {
         s3Service.deleteObject(key);
