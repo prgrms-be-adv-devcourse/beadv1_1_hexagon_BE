6d16a3c4ea58923874e83056bdebe3f3491aa6b7
sora0319 <sojeoung536985@gmail.com>
2025-12-08 03:59:00 +0900

    feat(post): 내부 서비스 문제 에러 코드 추가

--- cartpost-service/src/main/java/com/example/cartpostservice/common/exception/CustomStatusCode.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/common/exception/CustomStatusCode.java
@@ -18,9 +18,10 @@ public enum CustomStatusCode {
     NOT_FOUND_ITEM(HttpStatus.BAD_REQUEST, 6404, "아이템이 존재하지 않습니다"),
     NOT_FOUND_COMMISSION(HttpStatus.BAD_REQUEST, 6414, "의뢰글이 존재하지 않습니다"),
 
-    NOT_FOUND_MEMBER(HttpStatus.INTERNAL_SERVER_ERROR, 6500, "내부 서버 문제로 장바구니가 없습니다"),
-    NOT_FOUND_MEMBER_INFO(HttpStatus.INTERNAL_SERVER_ERROR, 6510, "내부 서버 문제로 사용자 정보가 없습니다"),
-    EXTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 6520, "외부 서버 통신 장애");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 6500, "내부 시스템에 오류가 발생했습니다"),
+    NOT_FOUND_MEMBER(HttpStatus.INTERNAL_SERVER_ERROR, 6510, "내부 서버 문제로 장바구니가 없습니다"),
+    NOT_FOUND_MEMBER_INFO(HttpStatus.INTERNAL_SERVER_ERROR, 6520, "내부 서버 문제로 사용자 정보가 없습니다"),
+    EXTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 6530, "외부 시스템과 통신 중 오류가 발생했습니다");
 
 
     private final HttpStatus status;
