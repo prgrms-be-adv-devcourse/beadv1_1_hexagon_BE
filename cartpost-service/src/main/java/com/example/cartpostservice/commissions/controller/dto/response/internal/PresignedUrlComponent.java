95627c8e071292929ad328b1d36a5658a3a37e2f
sora0319 <sojeoung536985@gmail.com>
2025-12-08 10:39:44 +0900

    feat(post): FileType 응답으로 추가

--- cartpost-service/src/main/java/com/example/cartpostservice/commissions/controller/dto/response/internal/PresignedUrlComponent.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/commissions/controller/dto/response/internal/PresignedUrlComponent.java
@@ -1,8 +1,11 @@
 package com.example.cartpostservice.commissions.controller.dto.response.internal;
 
+import org.hexagon.core.vo.FileType;
+
 public record PresignedUrlComponent(
         String key,
-        String queryString
+        String queryString,
+        FileType fileType
 ) {
 
 }
