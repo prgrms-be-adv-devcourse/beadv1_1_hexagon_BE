b39c62f0a3712896e4e3c428ad1c5d194f30237d
sora0319 <sojeoung536985@gmail.com>
2025-12-09 11:21:10 +0900

    feat(cart): path 설정으로 mapping 주소 변경

--- cartpost-service/src/main/java/com/example/cartpostservice/cart/controller/internal/ContractClient.java
+++ cartpost-service/src/main/java/com/example/cartpostservice/cart/controller/internal/ContractClient.java
@@ -12,15 +12,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "contract-service", path = "/internal/contracts", contextId = "ContractInfoClient") // 대상 서비스 이름 (Eureka 등록명 등)
+@FeignClient(name = "contract-service", path = "/internal/contracts", contextId = "ContractInfoClient")
+// 대상 서비스 이름 (Eureka 등록명 등)
 public interface ContractClient {
 
-    @PostMapping("/internal/contracts/pay")
+    @PostMapping("/pay")
     ResponseDto<List<ContractInfoResponse>> payContract(
             @RequestHeader("X-CODE") String xCode,
             @RequestBody ContractPayRequest request
     );
 
-    @GetMapping("/internal/contracts")
+    @GetMapping("")
     ResponseDto<List<ContractBriefWithNicknameResponse>> getBriefInfo(@RequestParam("code") List<String> codes);
 }
