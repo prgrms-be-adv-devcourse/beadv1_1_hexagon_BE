4deb50aff5f3c7b8d1109c5a9a512fd1ef9400dd
chahuigwang <chahuigwang@gmail.com>
2025-12-09 12:43:28 +0900

    chore(s3): S3 SDK 에러 발생 시 로깅 추가

--- s3-service/src/main/java/org/hexagon/s3service/exception/GlobalExceptionHandler.java
+++ s3-service/src/main/java/org/hexagon/s3service/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.hexagon.s3service.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hexagon.core.dto.Empty;
 import org.hexagon.core.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import software.amazon.awssdk.core.exception.SdkException;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(SdkException.class)
     public ResponseDto<Empty> handleException(SdkException e) {
+        log.error(e.getMessage(), e);
         return new ResponseDto<>(
                 5000,
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
