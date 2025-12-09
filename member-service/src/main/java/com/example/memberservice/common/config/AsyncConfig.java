5837560c54bcc7019c05d91c2b3d8d967f29be96
programofktw <thkim000317@naver.com>
2025-12-10 08:56:55 +0900

    feat(member): AsynConfig Executor 설정 추가

--- member-service/src/main/java/com/example/memberservice/common/config/AsyncConfig.java
+++ member-service/src/main/java/com/example/memberservice/common/config/AsyncConfig.java
@@ -1,9 +1,34 @@
 package com.example.memberservice.common.config;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
+    @Bean(name = "asyncExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("Async-");
+
+        // 백프레셔(backpressure) 위해 CallerRunsPolicy 추천
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return asyncExecutor();
+    }
 }
