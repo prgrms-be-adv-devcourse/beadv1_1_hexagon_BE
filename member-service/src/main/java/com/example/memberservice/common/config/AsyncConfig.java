03ddd9b52eb4809a32dfd2f7b2c5a6950c91a11b
programofktw <thkim000317@naver.com>
2025-12-09 23:48:54 +0900

    feat(member): async executor 설정

--- member-service/src/main/java/com/example/memberservice/common/config/AsyncConfig.java
+++ member-service/src/main/java/com/example/memberservice/common/config/AsyncConfig.java
@@ -1,9 +1,35 @@
 package com.example.memberservice.common.config;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Bean(name = "asyncExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(4);          // 기본 스레드 수
+        executor.setMaxPoolSize(10);           // 최대 스레드 수
+        executor.setQueueCapacity(32);        // 큐 용량
+        executor.setThreadNamePrefix("Async-"); // 스레드 이름 prefix
+
+
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return asyncExecutor(); // Async에서 사용할 Executor 지정
+    }
 }
