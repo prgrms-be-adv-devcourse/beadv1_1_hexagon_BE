97e2961a8cc3d8ee195bc08634cb73e8e1786409
ZZAMBAs <rksidksrksi@naver.com>
2025-12-12 10:04:06 +0900

    fix(contract): Feign basePackages 지정

--- contract-service/src/main/java/com/example/contractservice/common/config/FeignClientConfig.java
+++ contract-service/src/main/java/com/example/contractservice/common/config/FeignClientConfig.java
@@ -1,10 +1,11 @@
 package com.example.contractservice.common.config;
 
+import com.example.contractservice.ContractServiceApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients
+@EnableFeignClients(basePackageClasses = ContractServiceApplication.class)
 public class FeignClientConfig {
 
 }
