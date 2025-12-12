71c7e57e4be807bb118311da67a33608ef8bf758
chahuigwang <chahuigwang@gmail.com>
2025-12-12 10:15:05 +0900

    chore(search): HttpStatus 변경

--- search-service/src/main/java/com/example/searchservice/commission/exception/CommissionErrorCode.java
+++ search-service/src/main/java/com/example/searchservice/commission/exception/CommissionErrorCode.java
@@ -3,12 +3,13 @@ package com.example.searchservice.commission.exception;
 import com.example.searchservice.common.exception.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum CommissionErrorCode implements ErrorCode {
-    COMMISSION_PAY_FILTER_ERROR(5201, 500, "min-pay를 지정하기 위해서는 먼저 payment-type을 지정해야 합니다."),
-    COMMISSION_DATE_FILTER_ERROR(5202, 500, "ended-at은 반드시 start-at보다 이후의 날짜여야 합니다.");
+    COMMISSION_PAY_FILTER_ERROR(5201, HttpStatus.BAD_REQUEST.value(), "min-pay를 지정하기 위해서는 먼저 payment-type을 지정해야 합니다."),
+    COMMISSION_DATE_FILTER_ERROR(5202, HttpStatus.BAD_REQUEST.value(), "ended-at은 반드시 start-at보다 이후의 날짜여야 합니다.");
 
     private final int code;
     private final int httpStatus;
