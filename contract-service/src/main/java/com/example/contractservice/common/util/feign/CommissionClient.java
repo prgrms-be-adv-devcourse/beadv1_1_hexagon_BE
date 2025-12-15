eb712a609539ecef07d429edf332bb1a4a372ec3
ZZAMBAs <rksidksrksi@naver.com>
2025-12-16 08:59:57 +0900

    fix(contract): url에 / 빼먹은거 추가

--- contract-service/src/main/java/com/example/contractservice/common/util/feign/CommissionClient.java
+++ contract-service/src/main/java/com/example/contractservice/common/util/feign/CommissionClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "cartpost-service", path = "internal/commissions")
+@FeignClient(name = "cartpost-service", path = "/internal/commissions")
 public interface CommissionClient {
 
     @GetMapping("/recruitment-status/{commission-code}")
