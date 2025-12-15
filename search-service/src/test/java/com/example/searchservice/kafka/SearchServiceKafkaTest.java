8975872e71cf4cd99207f3d0d8d01026ab4da8ec
chahuigwang <chahuigwang@gmail.com>
2025-12-15 11:50:53 +0900

    feat(search): 테스트 코드에 upsertEvent, deletedEvent만 남김 + 테스트 완료

--- search-service/src/test/java/com/example/searchservice/kafka/SearchServiceKafkaTest.java
+++ search-service/src/test/java/com/example/searchservice/kafka/SearchServiceKafkaTest.java
@@ -2,31 +2,20 @@ package com.example.searchservice.kafka;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import com.example.searchservice.commission.entity.CommissionDocumentEntity;
 import com.example.searchservice.commission.repository.CommissionRepository;
 import com.example.searchservice.selfpromotion.repository.SelfPromotionRepository;
 import com.example.searchservice.tag.repository.TagRepository;
 import java.time.Instant;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import org.awaitility.Awaitility;
-import org.hexagon.core.events.commission.CommissionCreatedEvent;
+import org.hexagon.core.events.commission.CommissionUpsertEvent;
 import org.hexagon.core.events.commission.CommissionDeletedEvent;
-import org.hexagon.core.events.commission.CommissionInitEvent;
-import org.hexagon.core.events.commission.CommissionUpdatedEvent;
-import org.hexagon.core.events.selfpromotion.SelfPromotionCreatedEvent;
+import org.hexagon.core.events.selfpromotion.SelfPromotionUpsertEvent;
 import org.hexagon.core.events.selfpromotion.SelfPromotionDeletedEvent;
-import org.hexagon.core.events.selfpromotion.SelfPromotionInitEvent;
-import org.hexagon.core.events.selfpromotion.SelfPromotionUpdatedEvent;
-import org.hexagon.core.events.tag.TagInitEvent;
-import org.hexagon.core.vo.Commission;
 import org.hexagon.core.vo.PaymentType;
-import org.hexagon.core.vo.SelfPromotion;
-import org.hexagon.core.vo.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -66,136 +55,44 @@ class SearchServiceKafkaTest {
     private KafkaTemplate<String, Object> kafkaTemplate;
 
     @Autowired
-    private TagRepository tagRepository;
-
-    @Autowired
     private SelfPromotionRepository selfPromotionRepository;
 
     @Autowired
     private CommissionRepository commissionRepository;
 
     @Test
-    void tagInitEventTest() throws ExecutionException, InterruptedException {
+    void selfPromotionUpsertEventTest() throws ExecutionException, InterruptedException {
         // given
-        List<Tag> tags = List.of(
-                new Tag("test-001", "Spring"),
-                new Tag("test-002", "React"),
-                new Tag("test-003", "없는 기술명"));
-
-        TagInitEvent tagInitEvent = new TagInitEvent(tags);
-
-        // when
-        kafkaTemplate.send("tag-events", tagInitEvent).get();
-
-        // then
-        Awaitility.await().atMost(10, TimeUnit.SECONDS)
-                .untilAsserted(() -> {
-                    assertThat(tagRepository.existsById("test-001")).isTrue();
-                    assertThat(tagRepository.existsById("test-002")).isTrue();
-                    assertThat(tagRepository.existsById("test-003")).isTrue();
-                });
-
-        // tagRepository.deleteAll();
-    }
-
-    @Test
-    void selfPromotionInitEventTest() throws ExecutionException, InterruptedException {
-        // given
-        List<SelfPromotion> selfPromotions = List.of(
-                new SelfPromotion(
+        SelfPromotionUpsertEvent upsertEvent =
+                new SelfPromotionUpsertEvent(
                         "test-001",
-                        "Spring 개발자 구직",
-                        "Spring 개발자 구직 내용",
+                        "Kotlin 개발자 구직",
+                        "Kotlin 개발자 구직 내용",
                         "mem-001",
                         "닉네임1",
                         PaymentType.MONTHLY,
                         3_000_000L,
                         Instant.now()
-                ),
-                new SelfPromotion(
-                        "test-002",
-                        "React 개발자 구직",
-                        "React 개발자 구직 내용",
-                        "mem-002",
-                        "닉네임2",
-                        PaymentType.MONTHLY,
-                        3_000_000L,
-                        Instant.now()
-                )
-        );
-
-        SelfPromotionInitEvent selfPromotionInitEvent = new SelfPromotionInitEvent(selfPromotions);
+                );
 
         // when
-        kafkaTemplate.send("selfpromotion-events", selfPromotionInitEvent).get();
+        kafkaTemplate.send("selfpromotion-events", upsertEvent).get();
 
         // then
         Awaitility.await().atMost(10, TimeUnit.SECONDS)
                 .untilAsserted(() -> {
                     assertThat(selfPromotionRepository.existsById("test-001")).isTrue();
-                    assertThat(selfPromotionRepository.existsById("test-002")).isTrue();
-                });
-
-    }
-
-    @Test
-    void selfPromotionCreatedEventTest() throws ExecutionException, InterruptedException {
-        // given
-        SelfPromotionCreatedEvent selfPromotionCreatedEvent =
-                new SelfPromotionCreatedEvent(
-                        "test-003",
-                        "Kotlin 개발자 구직",
-                        "Kotlin 개발자 구직 내용",
-                        "mem-003",
-                        "닉네임3",
-                        PaymentType.MONTHLY,
-                        3_000_000L,
-                        Instant.now()
-                );
-
-        // when
-        kafkaTemplate.send("selfpromotion-events", selfPromotionCreatedEvent).get();
-
-        // then
-        Awaitility.await().atMost(10, TimeUnit.SECONDS)
-                .untilAsserted(() -> {
-                    assertThat(selfPromotionRepository.existsById("test-003")).isTrue();
-                });
-    }
-
-    @Test
-    void selfPromotionUpdatedEventTest() throws ExecutionException, InterruptedException {
-        // given
-        SelfPromotionUpdatedEvent selfPromotionUpdatedEvent =
-                new SelfPromotionUpdatedEvent(
-                        "test-003",
-                        "Python 개발자 구직",
-                        "Python 개발자 구직 내용",
-                        "mem-003",
-                        "닉네임3",
-                        PaymentType.MONTHLY,
-                        2_500_000L,
-                        Instant.now()
-                );
-
-        // when
-        kafkaTemplate.send("selfpromotion-events", selfPromotionUpdatedEvent).get();
-
-        // then
-        Awaitility.await().atMost(10, TimeUnit.SECONDS)
-                .untilAsserted(() -> {
-                    assertThat(selfPromotionRepository.findById("test-003").get().getTitle()).isEqualTo("Python 개발자 구직");
                 });
     }
 
     @Test
     void selfPromotionDeletedEventTest() throws ExecutionException, InterruptedException {
         // given
-        SelfPromotionDeletedEvent selfPromotionDeletedEvent =
+        SelfPromotionDeletedEvent deletedEvent =
                 new SelfPromotionDeletedEvent("test-001");
 
         // when
-        kafkaTemplate.send("selfpromotion-events", selfPromotionDeletedEvent).get();
+        kafkaTemplate.send("selfpromotion-events", deletedEvent).get();
 
         // then
         Awaitility.await().atMost(10, TimeUnit.SECONDS)
@@ -205,61 +102,14 @@ class SearchServiceKafkaTest {
     }
 
     @Test
-    void commissionInitEvent() throws ExecutionException, InterruptedException {
+    void commissionUpsertEventTest() throws ExecutionException, InterruptedException {
         // given
-        List<Commission> commissions = List.of(
-                new Commission(
-                        "test-001",
-                        "Spring 개발자 구인",
-                        "Spring 개발자 구인 내용",
-                        "mem-001",
-                        "닉네임1",
-                        List.of("Spring", "Backend"),
-                        LocalDate.now().minusDays(1),
-                        LocalDate.now().plusDays(7),
-                        PaymentType.MONTHLY,
-                        2_000_000L,
-                        false,
-                        Instant.now()
-                ),
-                new Commission(
-                        "test-002",
-                        "React 개발자 구인",
-                        "React 개발자 구인 내용",
-                        "mem-002",
-                        "닉네임2",
-                        List.of("React", "FrontEnd"),
-                        LocalDate.now(),
-                        LocalDate.now(),
-                        PaymentType.MONTHLY,
-                        2_000_000L,
-                        false,
-                        Instant.now()
-                )
-        );
-
-        CommissionInitEvent initEvent = new CommissionInitEvent(commissions);
-
-        // when
-        kafkaTemplate.send("commission-events", initEvent).get();
-
-        // then
-        Awaitility.await().atMost(10, TimeUnit.SECONDS)
-                .untilAsserted(() -> {
-                    assertThat(commissionRepository.existsById("test-001")).isTrue();
-                    assertThat(commissionRepository.existsById("test-002")).isTrue();
-                });
-    }
-
-    @Test
-    void commissionCreatedEventTest() throws ExecutionException, InterruptedException {
-        // given
-        CommissionCreatedEvent createdEvent = new CommissionCreatedEvent(
-                "test-003",
+        CommissionUpsertEvent upsertEvent = new CommissionUpsertEvent(
+                "test-001",
                 "Kotlin 개발자 구인",
                 "Kotlin 개발자 구인 내용",
-                "mem-003",
-                "닉네임3",
+                "mem-001",
+                "닉네임1",
                 List.of("Kotlin"),
                 LocalDate.now(),
                 LocalDate.now(),
@@ -270,41 +120,13 @@ class SearchServiceKafkaTest {
         );
 
         // when
-        kafkaTemplate.send("commission-events", createdEvent).get();
+        kafkaTemplate.send("commission-events", upsertEvent).get();
 
         // then
         Awaitility.await().atMost(10, TimeUnit.SECONDS)
                 .untilAsserted(() ->
-                        assertThat(commissionRepository.existsById("test-003")).isTrue()
+                        assertThat(commissionRepository.existsById("test-001")).isTrue()
                 );
-    }
-
-    @Test
-    void commissionUpdatedEventTest() throws ExecutionException, InterruptedException {
-        // given
-        CommissionUpdatedEvent updatedEvent = new CommissionUpdatedEvent(
-                "test-003",
-                "Python 개발자 구인",
-                "Python 개발자 구인 내용",
-                "mem-003",
-                "닉네임3",
-                List.of("Kotlin"),
-                LocalDate.now(),
-                LocalDate.now(),
-                PaymentType.MONTHLY,
-                2_500_000L,
-                false,
-                Instant.now()
-        );
-
-        // when
-        kafkaTemplate.send("commission-events", updatedEvent).get();
-
-        // then
-        Awaitility.await().atMost(10, TimeUnit.SECONDS)
-                .untilAsserted(() -> {
-                    assertThat(commissionRepository.findById("test-003").get().getTitle()).isEqualTo("Python 개발자 구인");
-                });
     }
 
     @Test
