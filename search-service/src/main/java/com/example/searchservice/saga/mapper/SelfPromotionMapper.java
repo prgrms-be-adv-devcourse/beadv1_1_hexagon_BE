c7801c55ceaed6f6decd01c70eb1053d6832a545
chahuigwang <chahuigwang@gmail.com>
2025-12-15 11:28:40 +0900

    feat(search): SelfPromotionMapper 내 사용하지 않는 메소드 삭제 및 upsertEvent -> Document 변환 메소드 구현

--- search-service/src/main/java/com/example/searchservice/saga/mapper/SelfPromotionMapper.java
+++ search-service/src/main/java/com/example/searchservice/saga/mapper/SelfPromotionMapper.java
@@ -1,45 +1,20 @@
 package com.example.searchservice.saga.mapper;
 
 import com.example.searchservice.selfpromotion.entity.SelfPromotionDocumentEntity;
-import org.hexagon.core.events.selfpromotion.SelfPromotionCreatedEvent;
-import org.hexagon.core.events.selfpromotion.SelfPromotionUpdatedEvent;
+import org.hexagon.core.events.selfpromotion.SelfPromotionUpsertEvent;
 import org.hexagon.core.vo.SelfPromotion;
 
 public class SelfPromotionMapper {
 
-    public static SelfPromotionDocumentEntity toSelfPromotionDocument(SelfPromotion selfPromotion) {
+    public static SelfPromotionDocumentEntity toSelfPromotionDocument(SelfPromotionUpsertEvent upsertEvent) {
         return SelfPromotionDocumentEntity.builder()
-                .code(selfPromotion.code())
-                .title(selfPromotion.title())
-                .content(selfPromotion.content())
-                .memberNickname(selfPromotion.memberNickname())
-                .paymentType(selfPromotion.paymentType())
-                .payAmount(selfPromotion.payAmount())
-                .updatedAt(selfPromotion.updatedAt())
-                .build();
-    }
-
-    public static SelfPromotionDocumentEntity toSelfPromotionDocument(SelfPromotionCreatedEvent createdEvent) {
-        return SelfPromotionDocumentEntity.builder()
-                .code(createdEvent.code())
-                .title(createdEvent.title())
-                .content(createdEvent.content())
-                .memberNickname(createdEvent.memberNickname())
-                .paymentType(createdEvent.paymentType())
-                .payAmount(createdEvent.payAmount())
-                .updatedAt(createdEvent.updatedAt())
-                .build();
-    }
-
-    public static SelfPromotionDocumentEntity toSelfPromotionDocument(SelfPromotionUpdatedEvent updatedEvent) {
-        return SelfPromotionDocumentEntity.builder()
-                .code(updatedEvent.code())
-                .title(updatedEvent.title())
-                .content(updatedEvent.content())
-                .memberNickname(updatedEvent.memberNickname())
-                .paymentType(updatedEvent.paymentType())
-                .payAmount(updatedEvent.payAmount())
-                .updatedAt(updatedEvent.updatedAt())
+                .code(upsertEvent.code())
+                .title(upsertEvent.title())
+                .content(upsertEvent.content())
+                .memberNickname(upsertEvent.memberNickname())
+                .paymentType(upsertEvent.paymentType())
+                .payAmount(upsertEvent.payAmount())
+                .updatedAt(upsertEvent.updatedAt())
                 .build();
     }
 }
