94664afd1cc54e66c2df058f18a5c8a7625c1014
chahuigwang <chahuigwang@gmail.com>
2025-12-16 12:20:07 +0900

    chore(search): commission 필드명 수정

--- search-service/src/main/java/com/example/searchservice/commission/entity/CommissionDocumentEntity.java
+++ search-service/src/main/java/com/example/searchservice/commission/entity/CommissionDocumentEntity.java
@@ -42,10 +42,10 @@ public class CommissionDocumentEntity {
     @Field(type = FieldType.Text, analyzer = "commission_index_analyzer", searchAnalyzer = "commission_search_analyzer")
     private String content;
 
-    @Field(type = FieldType.Keyword)
+    @Field(type = FieldType.Keyword, name = "member_code")
     private String memberCode;
 
-    @Field(type = FieldType.Keyword, name = "member_nickname")
+    @Field(type = FieldType.Keyword, name = "writer_name")
     private String memberNickname;
 
     @Field(type = FieldType.Keyword)
