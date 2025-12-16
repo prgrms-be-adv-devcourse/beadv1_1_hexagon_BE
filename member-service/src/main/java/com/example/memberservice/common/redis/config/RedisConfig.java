13ce0ed41791f490e93a263f99e882ea43cce197
programofktw <thkim000317@naver.com>
2025-12-16 11:47:41 +0900

    test(ci-cd): member 모듈 재 배포

--- member-service/src/main/java/com/example/memberservice/common/redis/config/RedisConfig.java
+++ member-service/src/main/java/com/example/memberservice/common/redis/config/RedisConfig.java
@@ -20,15 +20,12 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Value("${spring.data.redis.password}")
-    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
         config.setHostName(host);      // Redis 호스트
         config.setPort(port);          // Redis 포트
-        config.setPassword(RedisPassword.of(password)); // 비밀번호 설정
 
         return new LettuceConnectionFactory(config);
     }
