eed7e86ff0bc5393736eed8bd82cb720f36f4569
programofktw <thkim000317@naver.com>
2025-12-17 15:36:21 +0900

    fix(member): Request 변경

--- member-service/src/main/java/com/example/memberservice/member/controller/dto/request/MemberRoleUpdateRequest.java
+++ member-service/src/main/java/com/example/memberservice/member/controller/dto/request/MemberRoleUpdateRequest.java
@@ -4,9 +4,9 @@ import com.example.memberservice.member.model.enums.MemberRole;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
-@Schema(description = "추가할 멤버 상태를 담습니다.")
+@Schema(description = "변경할 멤버 상태를 담습니다.")
 public record MemberRoleUpdateRequest(
-    @Schema(description = "추가할 멤버 상태", allowableValues = {"CLIENT"})
+    @Schema(description = "변경 할 멤버 상태", allowableValues = {"CLIENT","FREELANCER"})
     MemberRole role
 ) {
 
