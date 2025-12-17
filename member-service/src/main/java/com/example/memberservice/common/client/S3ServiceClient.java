bd777ddb51426580445d32b9a4ab951e5cb03c2d
programofktw <thkim000317@naver.com>
2025-12-17 09:53:26 +0900

    fix(member): s3 service feignClient 수정

--- member-service/src/main/java/com/example/memberservice/common/client/S3ServiceClient.java
+++ member-service/src/main/java/com/example/memberservice/common/client/S3ServiceClient.java
@@ -4,12 +4,12 @@ package com.example.memberservice.common.client;
 import com.example.memberservice.common.client.dto.request.s3.PresignedDownloadRequestByCode;
 import com.example.memberservice.common.client.dto.request.s3.StoreKeysRequest;
 import com.example.memberservice.common.client.dto.response.s3.PresignedDownloadListResponse;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.hexagon.core.dto.Empty;
 import org.hexagon.core.dto.ResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
     name = "s3-service",
